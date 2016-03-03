ba2b5ed9b85fc25011d7f872d6c51646c5ce96c2
Scott Morton <mortonsc@umich.edu>
2016-03-02 21:17:33 -0500

    fix global variable initialization in crt0

--- src/tios_crt0.s
+++ src/tios_crt0.s
@@ -1,36 +1,76 @@
-;; The crt0 appears at the beginning of the compiled C program.
-;; This file is taken without any significant changes from an example
-;; posted by AHelper on the cemetech forum.
-;; (https://www.cemetech.net/forum/viewtopic.php?t=7087)
-
-; tios_crt0.s - TIOS assembly program header
+;--------------------------------------------------------------------------
+;  crt0.s - Generic crt0.s for a Z80
+;
+;  Copyright (C) 2000, Michael Hope
+;
+;  This library is free software; you can redistribute it and/or modify it
+;  under the terms of the GNU General Public License as published by the
+;  Free Software Foundation; either version 2, or (at your option) any
+;  later version.
+;
+;  This library is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License
+;  along with this library; see the file COPYING. If not, write to the
+;  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+;   MA 02110-1301, USA.
+;
+;  As a special exception, if you link this library with other files,
+;  some of which are compiled with SDCC, to produce an executable,
+;  this library does not by itself cause the resulting executable to
+;  be covered by the GNU General Public License. This exception does
+;  not however invalidate any other reasons why the executable file
+;   might be covered by the GNU General Public License.
+;--------------------------------------------------------------------------
+;  Modified 2 March 2016 by Scott Morton to be usable for TI83+
+;--------------------------------------------------------------------------
         .module crt0
         .globl _main
 
         .area _HEADER (ABS)
 
-        .org #0x9D93
-        .dw #0x6DBB
+        .org 0x9D93
+
+        ;; TI83+ expects to see this
+        .dw 0x6DBB
+
+	;; Initialise global variables
         call gsinit
         jp _main
 
         .org 0x9D9B
 
+	;; Ordering of segments for the linker.
         .area _HOME
         .area _CODE
+        .area _INITIALIZER
         .area _GSINIT
         .area _GSFINAL
+
         .area _DATA
+        .area _INITIALIZED
         .area _BSEG
         .area _BSS
         .area _HEAP
+
         .area _CODE
 
 __clock::
-        ld a,#2
-        ret ; needed somewhere...
+        ret
 
         .area _GSINIT
 gsinit::
-        .area _GSFINAL
-        ret
+	ld	bc, #l__INITIALIZER
+	ld	a, b
+	or	a, c
+	jr	Z, gsinit_next
+	ld	de, #s__INITIALIZED
+	ld	hl, #s__INITIALIZER
+	ldir
+gsinit_next:
+
+	.area   _GSFINAL
+	ret
