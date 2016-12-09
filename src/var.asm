93111fdd5377d2214fe1ad2d623fe8fa84c2ede7
Scott Morton <mortonsc@umich.edu>
2016-12-09 18:28:21 -0500

    make type-agnostic variable management routines

--- src/var.asm
+++ src/var.asm
@@ -31,110 +31,100 @@
 
         .area _CODE
 
-;; finds a program with the name given as a c-string
-;; inputs: hl = address of string containing name
-;; outputs: same as FindSym
-;; destroys: all
-FindPrgmH:
-        dec hl
-        rst rMOV9TOOP1
-        ld a,#ProgObj
-        ld (OP1),a
-        bcall _ChkFindSym
-        ret
-
 ;; void *CRecallPrgm(const uint8_t *name, uint16_t *size);
 _CRecallPrgm::
-        push ix
-        ld ix,#0
-        add ix,sp
-        ld l,4(ix)
-        ld h,5(ix)
-        call FindPrgmH
-        jr c,PrgmFailed
+        pop bc ;; return address 
+        pop hl ;; *name
+        pop de ;; *size
+        push de
+        push hl
+        push bc
+        ld a,#ProgObj
+        jmp RecallVar
+
+
+;; expects: *name in hl, var type token in a, pointer for size in de
+RecallVar:
+        push de
+        dec hl
+        rst rMOV9TOOP1
+        ld (OP1),a
+        bcall _ChkFindSym
+        jr c,RecallFailed
         ld a,b
         or a ; check if archived
-        jr z,PrgmInRam
-        AppOnErr UnarchivePrgmFailed
-        push ix ; _Arc_Unarc destroys all regs
-        rst rPUSHREALO1
+        jr z,VarInRam
+        AppOnErr UnarchiveFailed
+        rst rPUSHREALO1     ; not sure if _Arc_Unarc destroys OP1
         bcall _Arc_Unarc
         bcall _PopRealO1
-        pop ix
         AppOffErr
-        bcall _ChkFindSym ; find the new address
-PrgmInRam:
-        ex de,hl ; move address to hl
-        ld e,6(ix)
-        ld d,7(ix)
-        ldi ; store size in second argument
-        ldi ; and return the address of actual contents
-        pop ix
+        bcall _ChkFindSym   ; find the new address
+                            ; _Arc_Unarc maybe does this for us?
+VarInRam:
+        pop hl      ; get *size
+        ex de,hl    ; move address to hl (return value)
+        ldi         ; store size in second argument
+        ldi
         ret
-UnarchivePrgmFailed:
+UnarchiveFailed:
         bcall _PopRealO1
-        pop ix
-PrgmFailed:
+RecallFailed:
+        pop hl
         ld hl,#0; return null for var address
-        pop ix
         ret
-
-;; CreatePrgm and CreateProtPrgm are nearly identical, so they jump to a
-;; shared body, and push a flag to indicate what kind of program to make.
 
 ;; void *CCreatePrgm(const uint8_t *name, uint16_t size);
-_CCreatePrgm::
-        push ix
-        ld ix,#0
-        add ix,sp
-        xor a
-        inc a  ; reset zero flag
-        push af
-        jr CreatePrgm
+_CCreatePrgm
+        ld hl,#_CreateProg
+        ld (CreateVar),hl
+        pop de ;; return address 
+        pop hl ;; *name
+        pop bc ;; size
+        push bc
+        push hl
+        push de
+        ld a,#ProgObj
+        jmp CreateVar
 
 ;; void *CCreateProtPrgm(const uint8_t *name, uint16_t size);
-_CCreateProtPrgm::
-        push ix
-        ld ix,#0
-        add ix,sp
-        xor a  ; set zero flag
-        push af
-
-CreatePrgm:
-        ld l,4(ix)
-        ld h,5(ix)
-        call FindPrgmH
-        jr c,MakeNewPrgm
-        push ix ; DelVarArc destroys regs
-        bcall _DelVarArc ; delete the program if it exists
-        pop ix
-MakeNewPrgm:
-        pop af ; recall what kind of program to make
-        ld l,6(ix)
-        ld h,7(ix) ; load the desired size
-        push hl ; save the size
-        AppOnErr InsufficientMem
-        jr z,MakeProtPrgm
-        bcall _CreateProg
-        jr InitPrgm
-MakeProtPrgm:
+_CCreateProtPrgm
+        ld hl,#_CreateProtProg
+        ld (CreateVar),hl
+        pop de ;; return address 
+        pop hl ;; *name
+        pop bc ;; size
+        push bc
+        push hl
+        push de
         ld a,#ProtProgObj
+        jmp CreateVar
+
+;; expects: *name in hl, var type token in a, size in bc,
+;;          bcall for creating in CreateVar
+CreateVar:
+        push bc
+        dec hl
+        rst rMOV9TOOP1
         ld (OP1),a
-        bcall _CreateProtProg
-InitPrgm:
+        bcall _ChkFindSym
+        jr c,MakeNewVar
+        bcall _DelVarArc    ; delete the AppVar if it exists
+MakeNewVar:
+        pop bc
+        AppOnErr InsufficientMem
+        rst rBR_CALL        ; call bcall stored in next two bytes
+CreateVar:
+        .dw #_JError        ; should be overwritten
         AppOffErr
-        pop hl
-        ex de,hl ; now hl contains address, de contains size
-        ld (hl),e ; store the size in the first two bytes of the new object
+        ex de,hl ; now hl contains address
+        ld (hl),c ; store the size in the first two bytes of the new object
         inc hl
-        ld (hl),d
+        ld (hl),b
         inc hl ; return the address of the data
-        pop ix
         ret
 InsufficientMem:
-        pop hl
-        ld hl,#0 ; failed to create program, return null
-        pop ix
+        ld hl,#0 ; failed to create var, so return null
         ret
 
 ;; void CArchivePrgm(const uint8_t *name);
@@ -158,14 +148,20 @@ ArchivePrgmRet:
 
 ;; void CDeletePrgm(const uint8_t *name);
 _CDeletePrgm::
-        push ix
-        ld ix,#0
-        add ix,sp
-        ld l,4(ix)
-        ld h,5(ix)
-        call FindPrgmH
-        jr c,DeletePrgmRet ; var doesn't exist
+        pop bc ;; return address 
+        pop hl ;; *name
+        push hl
+        push bc
+        ld a,#ProgObj
+        jmp RecallVar
+
+;;  expects: *name in hl, var type token in a
+DeleteVar::
+        dec hl
+        rst rMOV9TOOP1
+        ld (OP1),a
+        bcall _ChkFindSym
+        jr c,DeletePrgmRet ; var doesn't exist (maybe not necessary?)
         bcall _DelVarArc
 DeletePrgmRet:
-        pop ix
         ret
